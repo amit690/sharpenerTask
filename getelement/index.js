49341d7461303cb2025ba5d4fcf5c215e20eb68f
amit690 <amitsanyal191@gmail.com>
2024-05-29 13:20:49 +0530

    creating first page

---
+++ getelement/index.js
@@ -0,0 +1,15 @@
+// Write your code below:
+document.getElementById('main-heading').textContent = 'Fruit World';
+        
+document.getElementById('main-heading').style.color = 'orange';
+
+document.getElementById('header').style.backgroundColor = 'green';
+
+document.getElementById('header').style.borderBottom = '2px solid orange';
+
+document.getElementById('basket-heading').style.color = 'green';
+
+const thanksDiv = document.getElementById('thanks');
+const thanksParagraph = document.createElement('p');
+thanksParagraph.textContent = 'Please visit us again';
+thanksDiv.appendChild(thanksParagraph);
